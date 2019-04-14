1e2018a9d97331702264ae9ed3678e7739d5bcaf
Joey Stoffels <joey.stoffels@exe.energy>
2019-04-14 22:08:57 +0200

    Working opdracht 1

--- sql/joey/opdracht-01.sql
+++ sql/joey/opdracht-01.sql
@@ -1,19 +1,35 @@
 USE odisee
 
-DECLARE @MovieInReeks INT = 313477;
+DECLARE @MovieInReeks INT = 207989;
 
-WITH result AS (
-	SELECT p.*
-	FROM Product p
-	WHERE product_id = @MovieInReeks
-	UNION ALL
-	SELECT child.*
-	FROM Product as child
-		JOIN result parent ON parent.previous_product_id = child.product_id
+WITH children AS (
+    SELECT p.*
+    FROM Product p
+    WHERE p.product_id = @MovieInReeks
 
+    UNION ALL
+
+    SELECT child.*
+    FROM Product as child
+      INNER JOIN children parent ON parent.previous_product_id = child.product_id
 )
+, parents AS (
+    SELECT p.*
+    FROM Product p
+    WHERE p.product_id = @MovieInReeks
 
-SELECT *
-FROM parent;
+    UNION ALL
 
--- TODO add volgnummers
+    SELECT super.*
+    FROM Product as super
+      INNER JOIN parents parent on parent.product_id = super.previous_product_id
+)
+SELECT product_id as PRODUCT_ID,
+	     title as TITLE,
+	     ROW_NUMBER() OVER (ORDER BY publication_year ASC) AS VOLGNUMMER
+FROM (
+    SELECT *
+    FROM children
+    UNION
+    SELECT *
+    FROM parents) as result;
