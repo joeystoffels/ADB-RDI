05bcecd232c481f2a05326dc4f68a5fe0230c9af
Marc Groenhout <m.groenhout@outlook.com>
2019-04-17 12:08:28 +0200

    Opdracht 1. Working with single CTE.

---
+++ sql/marc/opdracht-01.sql
@@ -0,0 +1,33 @@
+DECLARE @MovieInReeks INT = 207989; 
+
+WITH moviereeks 
+     AS (SELECT product_id, 
+                title, 
+                previous_product_id, 
+                publication_year 
+         FROM   product 
+         WHERE  product_id = @MovieInReeks 
+         UNION 
+         SELECT product_id, 
+                title, 
+                previous_product_id, 
+                publication_year 
+         FROM   product 
+         WHERE  previous_product_id = @MovieInReeks 
+         UNION ALL 
+         SELECT P.product_id, 
+                P.title, 
+                P.previous_product_id, 
+                P.publication_year 
+         FROM   product P 
+                JOIN moviereeks M 
+                  ON P.product_id = M.previous_product_id) 
+SELECT product_id                         AS ITEM_ID, 
+       title                              AS TITLE, 
+       Row_number() 
+         OVER ( 
+           ORDER BY publication_year ASC) AS Volgnummer 
+FROM   moviereeks 
+GROUP  BY product_id, 
+          title, 
+          publication_year 
