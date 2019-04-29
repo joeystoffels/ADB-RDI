125f829382f960a232dfdb8b9c6638787c552b20
Nick Hartjes <info@nickhartjes.nl>
2019-04-30 00:51:38 +0200

    Finished opdracht 2

--- sql/nick/opdracht-02.sql
+++ sql/nick/opdracht-02.sql
@@ -28,8 +28,9 @@ DECLARE    @Year INT = 2019;
     FROM Purchase AS P
              INNER JOIN [User] AS U ON P.email_address = U.email_address
     GROUP BY YEAR(P.purchase_date), MONTH(P.purchase_date), U.country_name
+    HAVING U.country_name = @Country
+       AND ((YEAR(P.purchase_date)  = @Year AND  MONTH(P.purchase_date) <= @Month) OR (YEAR(P.purchase_date)  = (@Year - 1) AND  MONTH(P.purchase_date) > @Month))
 )
-
  SELECT
      YMT1.Year,
      YMT1.Month,
@@ -37,8 +38,7 @@ DECLARE    @Year INT = 2019;
      FORMAT(YMT1.ItemsPerMonth / SUM(YMT1.ItemsPerMonth), 'P') AS PercentageOfTotal
  FROM YearMonthCountryTotals AS YMT1
  GROUP BY YMT1.Year,YMT1.Month, YMT1.ItemsPerMonth, YMT1.Country
- HAVING YMT1.Country = @Country
-AND ((YMT1.Year = @Year AND  YMT1.Month <= @Month) OR (YMT1.Year = (@Year - 1) AND  YMT1.Month > @Month))
+
 
 
 
