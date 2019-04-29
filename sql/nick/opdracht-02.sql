0c6a0bc3b2b77eb545152771e0124e1d5d2c73c4
Nick Hartjes <info@nickhartjes.nl>
2019-04-30 00:42:44 +0200

    Opdracht 2, de extra opdracht in 2 versies.

--- sql/nick/opdracht-02.sql
+++ sql/nick/opdracht-02.sql
@@ -56,6 +56,11 @@ AND ((YMT1.Year = @Year AND  YMT1.Month <= @Month) OR (YMT1.Year = (@Year - 1) A
 -- Maak ook deze tweede versie als je voor een 10 in aanmerking wilt komen.
 
 
+
+--------------------------------
+--     Optie 1
+--------------------------------
+
 DECLARE    @Year INT = 2019;
 
 ;WITH YearMonthCountryTotals (Countryname, Month, ItemsPerMonth) AS (
@@ -76,8 +81,8 @@ SELECT
        FORMAT( ISNULL(SUM(A.April), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS April,
        FORMAT( ISNULL(SUM(A.May), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS May,
        FORMAT( ISNULL(SUM(A.June), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS June,
-       FORMAT( ISNULL(SUM(A.July), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS May,
-       FORMAT( ISNULL(SUM(A.August), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS July,
+       FORMAT( ISNULL(SUM(A.July), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS July,
+       FORMAT( ISNULL(SUM(A.August), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS August,
        FORMAT( ISNULL(SUM(A.September), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS September,
        FORMAT( ISNULL(SUM(A.October), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS October,
        FORMAT( ISNULL(SUM(A.November), 0) / ISNULL(SUM(ItemsPerMonth),0), 'P') AS November,
@@ -125,3 +130,54 @@ SELECT
              FROM YearMonthCountryTotals
          ) AS A
 GROUP BY  A.Countryname
+
+
+--------------------------------
+--     Optie 2
+--------------------------------
+DECLARE    @Year INT = 2019;
+
+;WITH YearCountryTotals (Year, Country, ItemsPerMonth) AS (
+    SELECT YEAR(P.purchase_date)  AS Year,
+           U.country_name         AS Country,
+           COUNT(*)               AS ItemsPerMonth
+    FROM Purchase AS P
+             INNER JOIN [User] AS U ON P.email_address = U.email_address
+    GROUP BY YEAR(P.purchase_date), U.country_name
+    HAVING YEAR(P.purchase_date) = @Year
+),
+      YearMonthCountryTotals (Countryname, Month, ItemsPerMonth, TotalItems) AS (
+          SELECT
+              U.country_name               AS Countryname,
+              MONTH(P.purchase_date)       AS Month,
+              COUNT(*)                     AS ItemsPerMonth,
+              ( SELECT SUM(ItemsPerMonth)
+                FROM YearCountryTotals AS YCT
+                WHERE YCT.Country = U.country_name
+              )  AS TotalItems
+          FROM Purchase AS P
+                   INNER JOIN [User] AS U ON P.email_address = U.email_address
+          GROUP BY YEAR(P.purchase_date), MONTH(P.purchase_date), U.country_name
+          HAVING YEAR(P.purchase_date) = @Year
+      )
+
+SELECT
+       Countryname,
+       FORMAT( ISNULL([1], 0) / ISNULL(TotalItems, 0), 'P') AS January,
+       FORMAT( ISNULL([2], 0) / ISNULL(TotalItems, 0), 'P') AS February,
+       FORMAT( ISNULL([3], 0) / ISNULL(TotalItems, 0), 'P') AS March,
+       FORMAT( ISNULL([4], 0) / ISNULL(TotalItems, 0), 'P') AS April,
+       FORMAT( ISNULL([5], 0) / ISNULL(TotalItems, 0), 'P') AS May,
+       FORMAT( ISNULL([6], 0) / ISNULL(TotalItems, 0), 'P') AS June,
+       FORMAT( ISNULL([7], 0) / ISNULL(TotalItems, 0), 'P') AS July,
+       FORMAT( ISNULL([8], 0) / ISNULL(TotalItems, 0), 'P') AS August,
+       FORMAT( ISNULL([9], 0) / ISNULL(TotalItems, 0), 'P') AS September,
+       FORMAT( ISNULL([10], 0) / ISNULL(TotalItems, 0), 'P') AS October,
+       FORMAT( ISNULL([11], 0) / ISNULL(TotalItems, 0), 'P') AS November,
+       FORMAT( ISNULL([12], 0) / ISNULL(TotalItems, 0), 'P') AS December,
+       TotalItems
+FROM YearMonthCountryTotals
+PIVOT (
+        SUM(ItemsPerMonth)
+        FOR Month IN ([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12])
+    ) AS P
