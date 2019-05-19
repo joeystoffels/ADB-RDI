ff268dd40ef999f0e858fbdce1e682aa2cebd7c6
Marc Groenhout <m.groenhout@outlook.com>
2019-05-19 14:46:47 +0200

    Inital commit. 6th constraint.

---
+++ sql/marc/opdracht-constraints-6.sql
@@ -0,0 +1,12 @@
+/* 
+    Opdracht Constraints
+    Implementeer onderstaande constraints. Het kan zijn dat je een aantal al tijdens de casus DDDQ heb geïdentificeerd en/of zelfs al gemaakt. 
+	Lever in dat geval die code weer in voor deze casus. Indien je de constraints procedureel oplost, maak je zowel een Stored Procedure als een After Trigger. 
+	
+	Zorg voor nette error handling in jouw code en schrijf een complete testset voor beide constraint implementaties. 
+	Leg uit of de SP of Trigger jouw voorkeur heeft en uiteraard waarom. Als je bij een constraint vindt dat een Instead Of Trigger de betere variant is, 
+	maak je die ook en leg je uit waarom dit de beste oplossing is voor deze constraint.
+
+	6) Genres voor films en spellen zijn verschillend, deze mogen niet bij het verkeerde media-item gebruikt worden. 
+	   Hetzelfde geld voor Review aspecten.
+*/
