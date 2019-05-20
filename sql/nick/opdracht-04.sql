408b660730b045a2eee8060c6c0173c552ff59c3
Nick Hartjes <info@nickhartjes.nl>
2019-05-20 11:50:25 +0200

    Opdracht  4-5

---
+++ sql/nick/opdracht-04.sql
@@ -0,0 +1,39 @@
+-- 4.	In de oorspronkelijke data van IMDB staat in Imported_Director_Genre een veld Prob.
+-- Dit geeft de kans dat een film in deze database van dat genre is.
+-- Onderstaande query geeft de resultaten voor Sergio Leone.
+
+-- SELECT *
+-- FROM myimdb.dbo.Imported_Director_Genre
+-- WHERE did = 46046
+-- ORDER BY Prob
+--
+-- Staan deze gegevens in de juiste volgorde? Maak een query die deze gegevens produceert vanuit jouw eigen database op volgorde van kans, met de grootste kans bovenaan.
+
+
+SELECT P2.first_name, P2.last_name
+    FROM Movie_Director
+    INNER JOIN Product P on Movie_Director.product_id = P.product_id
+    INNER JOIN Person P2 on Movie_Director.person_id = P2.person_id
+
+
+SELECT *
+FROM Person
+WHERE  EXISTS  (
+    SELECT person_id
+    FROM Movie_Director
+)
+
+;WITH GenreMoviesPerDirectory AS (
+    SELECT P2.person_id, P2.first_name, P2.last_name, PG.genre_name, COUNT(PG.genre_name) AS TotalGenre, SUM(   )
+    FROM Movie_Director
+             INNER JOIN Product P on Movie_Director.product_id = P.product_id
+             INNER JOIN Person P2 on Movie_Director.person_id = P2.person_id
+             INNER JOIN Product_Genre PG on P.product_id = PG.product_id
+    GROUP BY P2.person_id, P2.first_name, P2.last_name, PG.genre_name
+)
+
+SELECT person_id, first_name, last_name , genre_name, MAX(Total), SUM(Total)
+FROM GenreMoviesPerDirectory
+GROUP BY person_id, first_name, last_name, genre_name
+HAVING Total = MAX(Total)
+
---
+++ sql/nick/opdracht-05.sql
@@ -0,0 +1,97 @@
+-- 5.	Vermoedelijk zijn niet alle Film ID’s in gebruik.
+-- Geef het statement dat de langste aaneengesloten reeks geeft van Film ID’s in jouw database.
+-- Geef ook het statement dat de langste reeks geeft die NIET in de database aanwezig is. Geef van beide statements het start en eind ID van de reeks.
+
+-- https://www.red-gate.com/simple-talk/sql/t-sql-programming/the-sql-of-gaps-and-islands-in-sequences/
+
+------------------------------------------
+-- Langste aaneengesloten reeks
+------------------------------------------
+;WITH DetectIslands AS (
+    SELECT
+       ROW_NUMBER() OVER (ORDER BY product_id) AS rn,
+       product_id,
+       product_id - ROW_NUMBER() OVER (ORDER BY product_id) AS diff
+    FROM Product
+), Islands AS (
+    SELECT MIN(product_id) AS [startID],
+           MAX(product_id) AS [endID],
+           MAX(product_id) - MIN(product_id) AS length
+    FROM DetectIslands
+    GROUP BY diff
+)
+SELECT
+       startID,
+       endID,
+       length
+FROM Islands
+WHERE length = (
+    SELECT MAX(length) FROM Islands
+)
+
+
+
+------------------------------------------
+-- Langste aaneengesloten reeks die NIET aanwezig is
+------------------------------------------
+
+;WITH DetectIslands AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY product_id) AS rn,
+        product_id,
+        product_id - ROW_NUMBER() OVER (ORDER BY product_id) AS diff
+    FROM Product
+), Islands AS (
+     SELECT
+        MIN(product_id) AS [startID],
+        MAX(product_id) AS [endID]
+--      MAX(product_id) - MIN(product_id) AS length
+     FROM DetectIslands
+     GROUP BY diff
+     )
+SELECT *
+FROM Islands AS I
+
+--     ;WITH DetectIslands AS (
+--         SELECT
+--     --         ROW_NUMBER() OVER (ORDER BY product_id) AS rn,
+--             product_id,
+--             product_id - ROW_NUMBER() OVER (ORDER BY product_id) AS rn
+--         FROM Product
+--     ), Islands AS (
+--         SELECT
+--     --            ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY MIN(rn)) AS grp,
+--                MIN(product_id) AS [startID],
+--                MAX(product_id) AS [endID]
+--         FROM DetectIslands
+--         GROUP BY product_id, rn
+--     )
+--     SELECT *
+--         FROM Islands AS I
+--     CROSS APPLY (
+--         VALUES (I.rn, I.endID + 1),(I.rn - 1, I.startID - 1)
+--     ) AS f(Seq, Num)
+--
+-- WITH cteSource(ID, Seq, Num)
+--          AS (
+--         SELECT d.ID, f.Seq, f.Num
+--         FROM (
+--                  SELECT ID,
+--                         ROW_NUMBER() OVER (PARTITION BY ID ORDER BY MIN(SeqNo)) AS Grp,
+--                         MIN(SeqNo) AS StartSeqNo,
+--                         MAX(SeqNo) AS EndSeqNo
+--                  FROM (
+--                           SELECT ID, SeqNo,
+--                                  SeqNo - ROW_NUMBER() OVER (PARTITION BY ID ORDER BY SeqNo) AS rn
+--                           FROM dbo.GapsIslands
+--                       ) AS a
+--                  GROUP BY ID,rn
+--              ) d
+--                  CROSS APPLY (
+--             VALUES (d.Grp, d.EndSeqNo + 1),(d.Grp - 1, d.StartSeqNo - 1)
+--         ) AS f(Seq, Num)
+--     )
+-- SELECT ID, MIN(Num) AS StartSeqNo, MAX(Num) AS EndSeqNo
+-- FROM cteSource
+-- GROUP BY ID, Seq
+-- HAVING COUNT(*) = 2;
