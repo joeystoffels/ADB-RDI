1e58d010e63ba75fe0b231bea650d992671c56c1
Nick Hartjes <info@nickhartjes.nl>
2019-05-26 18:56:56 +0200

    Tmp save

--- sql/nick/opdracht-constraints-4.sql
+++ sql/nick/opdracht-constraints-4.sql
@@ -1,70 +1,95 @@
+--  --------------------------------------------------------
 -- Constraint 4:
 -- De datum waarop een film wordt bekeken valt binnen de/een abonnementperiode.
+--  --------------------------------------------------------
 
-USE odisee
+USE odisee;
+GO
+DROP TRIGGER IF EXISTS TR_WatchMovieInPeriod;
+GO
 
--- Scenario's
---
+--  --------------------------------------------------------
+--  Trigger
+--  --------------------------------------------------------
+CREATE TRIGGER TR_WatchMovieInPeriod ON Purchase
+AFTER INSERT, UPDATE
+AS
+     BEGIN
+         SET NOCOUNT ON; -- Stops the message that shows the count of the number of rows affected
+         -- Declare variables
+         DECLARE @email NVARCHAR(4000);
+         IF NOT EXISTS
+         (
+             SELECT *
+             FROM inserted
+         )
+             RETURN; -- If nothing is inserted
+         BEGIN TRY
+             SET @email =
+             (
+                 SELECT email_address
+                 FROM inserted
+             );
+             IF EXISTS
+             (
 
--- Testscenario's
+
+                 Select * from [User] AS U
+				 INNER JOIN User_Subscription AS US ON U.email_address = US.email_address
+				 where U.email_address = 'nickhartjes@gmail.com'
+             )
+                 THROW 50001, 'Beautifull error message, readable for users...', 1;
+         END TRY
+         BEGIN CATCH
+             THROW; -- Using TROW handles ROLLBACK and bubbles up the thrown error.
+         END CATCH;
+     END;
+	 GO
+
+--  --------------------------------------------------------
+--  Demo data
+--  --------------------------------------------------------
+INSERT INTO User_Subscription  VALUES ('nickhartjes@gmail.com', 'The Netherlands', 'Basic', 'Basic', '2018-12-01', '2019-05-07', 3.00);
+INSERT INTO User_Subscription  VALUES ('nickhartjes@gmail.com', 'The Netherlands', 'Basic', 'Basic', '2019-05-01', '2019-05-07', 3.00);
+INSERT INTO User_Subscription  VALUES ('nickhartjes@gmail.com', 'The Netherlands', 'Basic', 'Basic', '2019-05-01', '2019-05-07', 3.00);
+
+
+--  --------------------------------------------------------
+--  Testscenario's
+--  --------------------------------------------------------
 -- Film valt voor de abonnementperiode
 -- Film valt na de abonnementperiode
 -- Film valt gelijk aan de startdag van de abonnementperiode.
 -- Film valt gelijk aan de einddag van de abonnementperiode.
--- Film valt tussen een abbonementsperiode in
+-- Film valt tussen abbonementsperiode's in
+-- Film valt in een periode waarin er geen einddag bekend is.
+-- Film valt in meerdere abonnementperiodes
 
-CREATE TRIGGER dbo.Purchase.testTrigger
-    ON dbo.Purchase
-    AFTER INSERT, UPDATE
-    AS
-BEGIN
-    SET NOCOUNT ON;
-
-    ------- Declare variables
-    declare @email nvarchar(4000)
-
-    IF NOT EXISTS (SELECT * FROM inserted) RETURN; -- Speed is everything, nothing to do (for insert,update)? RETURN
-    BEGIN TRY
-
-        set @email = (select email_address from inserted)
-        /* put your checks here. Make sure they are SET based. Make sure you test every trigger
-           with statements with one and more records; mixing valid and invalid records.
-           This block usually starts with something like:
-        IF EXISTS(...) */
-        -- and if something (negative select) is found, throw an error.
-        THROW 50001, 'Beautifull error message, readable for users...', 1;
-    END TRY
-    BEGIN CATCH
-        THROW -- Using TROW handles ROLLBACK and bubbles up the thrown error.
-        /* If more control is necassary RAISERROR can still be used,
-           but Transaction management needs to be taken care of and
-           quircks are present. i.e. code after RAISERROR is still executed. */
-    END CATCH
-END
-
---
---
--- CREATE PROCEDURE <procedurename>
--- -- Variable list
--- AS
--- -- unless you're very sure you need XACT_ABORT or NOCOUNT OFF, set them ON by default!
--- SET NOCOUNT, XACT_ABORT ON
--- /* Procedure must start its own transaction (if necessary of course).*/
--- --SET TRANSACTION ISOLATION LEVEL ...
--- BEGIN TRANSACTION;
---
--- BEGIN TRY
---     /* Database actions executed here.*/
---
---     /* Get here if no errors. If under Transaction, we must commit the transaction */
---     COMMIT TRANSACTION;
--- END TRY
--- BEGIN CATCH
---     /* An error occurred. If under Transaction, ROLLBACK complete transaction;
---        this includes any outer transactions!
---        Take care with nested Transactions. Another (nested) Transaction could
---        have ROLLBACK-ed our Transaction, hence the XACT_STATE check. */
---     IF XACT_STATE() <> 0 ROLLBACK TRANSACTION;
---     THROW;
--- END CATCH
--- GO
+DECLARE @DATUM1 DATE;
+DECLARE @DATUM2 DATE;
+SELECT @DATUM1 = MIN(DateMarriage)
+FROM MARRIAGE
+WHERE HusbandID = 1
+      AND DateDivorce IS NULL;
+SELECT @DATUM2 = MIN(DateMarriage)
+FROM MARRIAGE
+WHERE HusbandID = 3
+      AND DateDivorce IS NULL;
+--eentje fout
+INSERT INTO Marriage
+VALUES
+(1, 
+ 3, 
+ DATEADD(day, -60, @DATUM1), 
+ DATEADD(day, -55, @DATUM1)
+),
+(3, 
+ 8, 
+ DATEADD(day, -10, @DATUM2), 
+ DATEADD(day, -8, @DATUM2)
+);
+--beiden goed
+--INSERT INTO Marriage 
+--	values (1,3, DATEADD(day, -48, @DATUM1), DATEADD(day, -30, @DATUM1)),
+--	(3,8, DATEADD(day, -49, @DATUM2), DATEADD(day, -40, @DATUM2));
+GO
