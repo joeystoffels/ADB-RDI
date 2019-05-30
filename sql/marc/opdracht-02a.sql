78db2ba09a4c8af2f195d3edffdd479742b1a714
Nick Hartjes <info@nickhartjes.nl>
2019-05-30 08:57:00 +0200

    First setup for the main document

---
+++ sql/marc/opdracht-02a.sql
@@ -0,0 +1,34 @@
+/* 
+    Opdracht 2.
+    Geef het statement dat per land het maandgebruik over 12 maanden geeft 
+    met daarbij het percentage dat die maand uitmaakt van het totaal van die 12 maanden.
+*/
+
+
+
+/*
+ VERSIE 1
+ Per land de afgelopen 12 maanden in rijen
+ */
+SELECT Year(P.purchase_date) AS Year
+	,Month(P.purchase_date) AS Month
+	,Count(*) AS ItemsPerMonth
+	,CONCAT (
+		CONVERT(DECIMAL(4, 2), (
+				100 / (
+					SELECT CONVERT(DECIMAL(4, 2), Count(*))
+					FROM Purchase P
+					INNER JOIN [User] AS U ON P.email_address = U.email_address
+					WHERE U.country_name = 'The Netherlands'
+					)
+				)) * Count(*)
+		,'%'
+		) AS PercentageOfTotal
+FROM Purchase P
+INNER JOIN [User] AS U ON P.email_address = U.email_address
+WHERE U.country_name = 'The Netherlands'
+	AND P.purchase_date BETWEEN '20180501'
+		AND '20190501'
+GROUP BY U.country_name
+	,Year(P.purchase_date)
+	,Month(P.purchase_date)
