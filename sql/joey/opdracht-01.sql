9f0de8c4779b5dfc22f92b285510d9136767db58
Nick Hartjes <info@nickhartjes.nl>
2019-05-30 09:22:10 +0200

    Queries formatted for Joey

--- sql/joey/opdracht-01.sql
+++ sql/joey/opdracht-01.sql
@@ -1,38 +1,33 @@
 -- Opdracht 1
 USE ODISEE;
 GO
-
-DECLARE @MovieInReeks INT = 207989
-
-;WITH children AS (
-    SELECT P.*
-    FROM Product P
-    WHERE P.product_id = @MovieInReeks
-
-    UNION ALL
-
-    SELECT child.*
-    FROM Product as child
-      INNER JOIN children parent ON parent.previous_product_id = child.product_id
-)
-, parents AS (
-    SELECT p.*
-    FROM Product p
-    WHERE p.product_id = @MovieInReeks
-
-    UNION ALL
-
-    SELECT super.*
-    FROM Product as super
-      INNER JOIN parents parent on parent.product_id = super.previous_product_id
-)
-SELECT product_id as PRODUCT_ID,
-	     title as TITLE,
-	     ROW_NUMBER() OVER (ORDER BY publication_year ASC) AS VOLGNUMMER
-FROM (
-    SELECT *
-    FROM children
-    UNION
-    SELECT *
-    FROM parents) as result;
+DECLARE @MovieInReeks INT= 207989;
+WITH children
+     AS (SELECT P.*
+         FROM Product P
+         WHERE P.product_id = @MovieInReeks
+         UNION ALL
+         SELECT child.*
+         FROM Product AS child
+              INNER JOIN children parent ON parent.previous_product_id = child.product_id),
+     parents
+     AS (SELECT p.*
+         FROM Product p
+         WHERE p.product_id = @MovieInReeks
+         UNION ALL
+         SELECT super.*
+         FROM Product AS super
+              INNER JOIN parents parent ON parent.product_id = super.previous_product_id)
+     SELECT product_id AS PRODUCT_ID, 
+            title AS TITLE, 
+            ROW_NUMBER() OVER(
+            ORDER BY publication_year ASC) AS VOLGNUMMER
+     FROM
+     (
+         SELECT *
+         FROM children
+         UNION
+         SELECT *
+         FROM parents
+     ) AS result;
 GO
--- sql/joey/opdracht-03.sql
+++ sql/joey/opdracht-03.sql
@@ -5,24 +5,31 @@
 
 USE ODISEE;
 GO
-
-SELECT Y.product_id, P.title, Y.MedianDisc, Y.MedianCont
-FROM (	
-		SELECT 	product_id,
-				score,
-				PERCENTILE_DISC(0.5) 
-					WITHIN GROUP (ORDER BY score)
-					OVER (PARTITION BY product_id) AS MedianDisc,
-				PERCENTILE_CONT(0.5) 
-					WITHIN GROUP (ORDER BY score)
-					OVER (PARTITION BY product_id) AS MedianCont,
-					ROW_NUMBER() OVER(ORDER BY product_id ASC) as 'Row'
-		FROM Review_Category R
-		WHERE EXISTS (SELECT product_id	FROM Purchase)
-	 ) AS Y
+SELECT Y.product_id, 
+       P.title, 
+       Y.MedianDisc, 
+       Y.MedianCont
+FROM
+(
+    SELECT product_id, 
+           score, 
+           PERCENTILE_DISC(0.5) WITHIN GROUP(ORDER BY score) OVER(PARTITION BY product_id) AS MedianDisc, 
+           PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY score) OVER(PARTITION BY product_id) AS MedianCont, 
+           ROW_NUMBER() OVER(
+           ORDER BY product_id ASC) AS 'Row'
+    FROM Review_Category R
+    WHERE EXISTS
+    (
+        SELECT product_id
+        FROM Purchase
+    )
+) AS Y
 JOIN Product P ON Y.product_id = P.product_id
 WHERE P.product_type = 'Movie'
-AND Row BETWEEN 0 AND 10
-GROUP BY Y.product_id, P.title, Y.MedianDisc, Y.MedianCont
+      AND Row BETWEEN 0 AND 10
+GROUP BY Y.product_id, 
+         P.title, 
+         Y.MedianDisc, 
+         Y.MedianCont
 ORDER BY Y.MedianDisc DESC;
 GO
