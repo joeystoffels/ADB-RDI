c02e41fe54026cad3aef3a7cac913acfc476ede3
Nick Hartjes <info@nickhartjes.nl>
2019-06-02 20:22:47 +0200

    Fixed missing where statement

--- sql/nick/opdracht-05a.sql
+++ sql/nick/opdracht-05a.sql
@@ -9,7 +9,8 @@ WITH DetectIslands
                 product_id, 
                 product_id - ROW_NUMBER() OVER(
                 ORDER BY product_id) AS diff
-         FROM Product),
+         FROM Product
+		 WHERE product_type = 'Movie'),
      Islands
      AS (SELECT MIN(product_id) AS [startID], 
                 MAX(product_id) AS [endID], 
@@ -30,16 +31,26 @@ SET STATISTICS IO OFF;
 GO
 
 
--- SQL Server Execution Times:
---    CPU time = 0 ms,  elapsed time = 0 ms.
 -- SQL Server parse and compile time:
---    CPU time = 7 ms, elapsed time = 7 ms.
---
--- (1 row affected)
--- Table 'Product'. Scan count 2, logical reads 1740, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
--- Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
--- Table 'Workfile'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
--- Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+--    CPU time = 0 ms, elapsed time = 0 ms.
 --
 --  SQL Server Execution Times:
---    CPU time = 1358 ms,  elapsed time = 795 ms.
+--    CPU time = 0 ms,  elapsed time = 0 ms.
+-- SQL Server parse and compile time:
+--    CPU time = 0 ms, elapsed time = 0 ms.
+--
+--  SQL Server Execution Times:
+--    CPU time = 0 ms,  elapsed time = 0 ms.
+--
+--  SQL Server Execution Times:
+--    CPU time = 0 ms,  elapsed time = 0 ms.
+-- SQL Server parse and compile time:
+--    CPU time = 18 ms, elapsed time = 18 ms.
+--
+-- (1 row affected)
+-- Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+-- Table 'Product'. Scan count 10, logical reads 6700, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+-- Table 'Workfile'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+--
+--  SQL Server Execution Times:
+--    CPU time = 2219 ms,  elapsed time = 1277 ms.
