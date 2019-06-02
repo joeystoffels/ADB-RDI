43e3121655d3bc25f4667d885fc5e85e1040a5c3
Joey Stoffels <joey.stoffels@exe.energy>
2019-06-02 12:18:11 +0200

    update tests

--- sql/joey/opdracht-constraints-2.sql
+++ sql/joey/opdracht-constraints-2.sql
@@ -9,9 +9,18 @@ AS
 BEGIN
 
 	SET NOCOUNT ON;
-	IF NOT EXISTS (SELECT product_type FROM inserted WHERE product_type = 'Movie' AND previous_product_id IS NOT NULL) RETURN;
+	IF EXISTS (SELECT product_type FROM inserted WHERE product_type != 'Movie' OR previous_product_id IS NULL) RETURN;
 
 	BEGIN TRY
+		PRINT 'In try block of TR_Products_AI_AU';
+
+		IF EXISTS (
+		SELECT *
+			FROM Product P
+			INNER JOIN inserted I ON P.product_id = I.previous_product_id
+			WHERE P.product_type != 'Movie')
+
+		THROW 50001, 'Previous part is not of type Movie', 1;
 
 		IF EXISTS (
 			SELECT *
@@ -19,7 +28,8 @@ BEGIN
 			INNER JOIN inserted I ON P.product_id = I.previous_product_id
 			WHERE P.publication_year <= I.publication_year)
 
-		THROW 50001, 'publication_year of previous_product_id is after the inserted/updated products publication_year!', 1;
+		THROW 50001, 'Publication_year of previous part is after the inserted/updated product publication year!', 1;
+
 	END TRY
 	BEGIN CATCH
 		PRINT 'In catch block of TR_Products_AI_AU';
@@ -34,11 +44,11 @@ END;
 -- before or after the current product, thus we assume that the same publication_year violates the trigger rules.
 
 
--- Should fail because its publication_year is before 1999.
+-- Should fail because its publication year is before 1999.
 INSERT INTO Product
 VALUES	(9999998, 'Movie', 345635, 'Star Wars Latest', null, null, 2.00, 2000, null, null, null)
 
--- Should succeed because its publication_year is after 1999.
+-- Should succeed because its publication year is after 1999.
 INSERT INTO Product
 VALUES	(9999999, 'Movie', 345635, 'Star Wars Latest', null, null, 2.00, 1998, null, null, null)
 
@@ -47,11 +57,11 @@ DELETE FROM PRODUCT WHERE product_id = 9999999
 
 
 
--- Should fail because its publication_year is before 1999 (2002).
+-- Should fail because its publication year is before 1999 (2002).
 UPDATE Product
 SET previous_product_id = 345635 WHERE product_id = 313503
 
--- Should succeed because its publication_year is after 1999 (1996).
+-- Should succeed because its publication year is after 1999 (1996).
 UPDATE Product
 SET previous_product_id = 345635 WHERE product_id = 313508
 
@@ -61,11 +71,11 @@ SET previous_product_id = null WHERE product_id = 313508
 
 
 
--- Should fail because it has the same publication_year.
+-- Should fail because it has the same publication year.
 INSERT INTO Product
 VALUES (9999999, 'Movie', 345635, 'Star Wars Latest', null, null, 2.00, 1999, null, null, null)
 
--- Should fail because it has the same publication_year.
+-- Should fail because it has the same publication year.
 UPDATE Product
 SET previous_product_id = 345635 WHERE product_id = 313799
 
@@ -87,5 +97,25 @@ VALUES	(9999999, 'Movie', 345635, 'Star Wars Latest', null, null, 2.00, 1989, nu
 		(9999998, 'Movie', 345635, 'Star Wars Latest', null, null, 2.00, 1988, null, null, null)
 
 -- Rollback
-DELETE FROM PRODUCT WHERE product_id = 9999999
-DELETE FROM PRODUCT WHERE product_id = 9999998
+DELETE FROM Product WHERE product_id = 9999999
+DELETE FROM Product WHERE product_id = 9999998
+
+
+
+-- Should fail because previous part is of type 'Game'.
+INSERT INTO Product
+VALUES (9999999, 'Movie', 412331, 'Star Wars Latest', null, null, 2.00, 1999, null, null, null)
+
+
+
+-- Should bypass trigger and succeed because product type is not 'Movie'.
+INSERT INTO Product
+VALUES (9999998, 'Game', 345635, 'Star Wars Latest', null, null, 2.00, 1999, null, null, null)
+
+-- Should bypass trigger and succeed because previous part is null.
+INSERT INTO Product
+VALUES (9999999, 'Movie', null, 'Star Wars Latest', null, null, 2.00, 1999, null, null, null)
+
+-- Rollback
+DELETE FROM Product WHERE product_id = 9999999
+DELETE FROM Product WHERE product_id = 9999998
