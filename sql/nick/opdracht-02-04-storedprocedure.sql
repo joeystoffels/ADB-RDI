7bc34d01e55e5048d2f90d612e33a31d39f5a98d
Nick Hartjes <info@nickhartjes.nl>
2019-07-01 21:54:06 +0200

    Stored procedure

--- sql/nick/opdracht-02-04-storedprocedure.sql
+++ sql/nick/opdracht-02-04-storedprocedure.sql
@@ -1,7 +1,8 @@
 USE odisee;
 GO
 DROP PROCEDURE IF EXISTS SP_PurchaseInsert;
-DROP PROCEDURE IF EXISTS SP_InsertDemoData; 
+DROP PROCEDURE IF EXISTS SP_InsertDemoData;
+DROP PROCEDURE IF EXISTS SP_DeleteDemoData;
 GO
 
 --  --------------------------------------------------------
@@ -14,31 +15,31 @@ CREATE PROCEDURE SP_PurchaseInsert
  @price         PRICE
 )
 AS
-    BEGIN
+     SET NOCOUNT, XACT_ABORT ON;
+    BEGIN TRANSACTION;
+    BEGIN TRY
+        IF NOT EXISTS
+        (
+            SELECT 'Subscription'
+            FROM User_Subscription AS US
+            WHERE @purchase_date BETWEEN US.subscription_startdate AND ISNULL(subscription_enddate, DATEADD(year, 100, @purchase_date))
+        )
+            THROW 50001, 'Product(s) can not be purchased outside a subscription period', 1;
+        INSERT INTO Purchase
+        VALUES
+        (@productId, 
+         @email_address, 
+         @purchase_date, 
+         @price
+        );
 
-        SET NOCOUNT ON;
-
-        SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
-	    BEGIN TRANSACTION;
-
-		 IF NOT EXISTS
-             (
-                 SELECT 'Subscription'
-                 FROM User_Subscription
-                 WHERE email_address = @email_address
-                       AND subscription_startdate <= @purchase_date
-                       AND ISNULL(subscription_enddate, '2099-12-31') >= @purchase_date
-             )
-		 BEGIN
-                RAISERROR('Product can not be purchased outside a subscription period', 16, 1);
-                ROLLBACK TRANSACTION;
-                RETURN;
-        END;
-
-        SET NOCOUNT OFF;
-    END;
-
-    COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        IF XACT_STATE() <> 0
+            ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
+     COMMIT TRANSACTION;
 GO
 
 --  --------------------------------------------------------
@@ -49,15 +50,9 @@ CREATE PROCEDURE SP_InsertDemoData
 AS
     BEGIN
         SET NOCOUNT ON;
-        DECLARE @email NVARCHAR(4000)= 'test@test.nl';
-        DECLARE @startDate01 DATE= '2017-03-01';
-        DECLARE @endDate01 DATE= '2018-03-01';
-        DECLARE @startDate02 DATE= '2018-05-01';
-        DECLARE @endDate02 DATE= '2019-05-01';
-        DECLARE @startDate03 DATE= '2019-06-01';
         INSERT INTO [User]
         VALUES
-        (@email, 
+        ('test@test.nl', 
          'The Netherlands', 
          'Test user', 
          'Delete', 
@@ -67,33 +62,36 @@ AS
         );
         INSERT INTO User_Subscription
         VALUES
-        (@email, 
+        ('test@test.nl', 
          'The Netherlands', 
          'Basic', 
          'Basic', 
-         @startDate01, 
-         @endDate01, 
+         '2017-03-01', 
+         '2018-03-01', 
          3.00
-        ),
-        (@email, 
+        );
+        INSERT INTO User_Subscription
+        VALUES
+        ('test@test.nl', 
          'The Netherlands', 
          'Basic', 
          'Basic', 
-         @startDate02, 
-         @endDate02, 
+         '2018-05-01', 
+         '2019-05-01', 
          3.00
-        ),
-        (@email, 
+        );
+        INSERT INTO User_Subscription
+        VALUES
+        ('test@test.nl', 
          'The Netherlands', 
          'Basic', 
          'Basic', 
-         @startDate03, 
+         '2019-06-01', 
          NULL, 
          3.00
         );
     END;
 GO
-
 CREATE PROCEDURE SP_DeleteDemoData
 AS
     BEGIN
@@ -104,14 +102,16 @@ AS
         DECLARE @startDate02 DATE= '2018-05-01';
         DECLARE @endDate02 DATE= '2019-05-01';
         DECLARE @startDate03 DATE= '2019-06-01';
-
-		DELETE FROM [User]
-		WHERE email_address = @email;
-
-		DELETE FROM User_Subscription
-		WHERE email_address = @email;
+        DELETE FROM Purchase
+        WHERE email_address = @email;
+        DELETE FROM User_Subscription
+        WHERE email_address = @email;
+        DELETE FROM [User]
+        WHERE email_address = @email;
     END;
 GO
+
+EXEC SP_DeleteDemoData;
 
 --  --------------------------------------------------------
 --  Testscenario's
@@ -129,78 +129,68 @@ GO
 -- [Scenario 01] : Film valt voor de abonnementperiode
 -- Result: Throw Error
 EXEC SP_InsertDemoData;
-EXEC SP_PurchaseInsert 1, 'test@test.nl', '2010-01-01', 3.2;
-ROLLBACK TRANSACTION;
+EXEC SP_PurchaseInsert 
+     1, 
+     'test@test.nl', 
+     '2010-01-01', 
+     3.2;
+EXEC SP_DeleteDemoData;
+
 
 -- [Scenario 02] : Film valt na de abonnementperiode
 -- Result: Throw Error
-BEGIN TRANSACTION;
 EXEC SP_InsertDemoData;
-INSERT INTO Purchase
-VALUES
-(123123, 
- 'test@test.nl', 
- '2019-05-15', 
- 3.00
-);
-ROLLBACK TRANSACTION;
+EXEC SP_PurchaseInsert
+     1,
+     'test@test.nl',
+     '2019-05-15',
+     3.2;
+EXEC SP_DeleteDemoData;
 
 -- [Scenario 03] : Film valt gelijk aan de startdag van de abonnementperiode.
 -- Result: Success
-BEGIN TRANSACTION;
 EXEC SP_InsertDemoData;
-INSERT INTO Purchase
-VALUES
-(123123, 
- 'test@test.nl', 
- '2017-03-01', 
- 3.00
-);
-ROLLBACK TRANSACTION;
+EXEC SP_PurchaseInsert
+     1,
+     'test@test.nl',
+     '2017-03-01',
+     3.2;
+EXEC SP_DeleteDemoData;
 
 -- [Scenario 04] : Film valt gelijk aan de einddag van de abonnementperiode.
 -- Result: Success
-BEGIN TRANSACTION;
 EXEC SP_InsertDemoData;
-INSERT INTO Purchase
-VALUES
-(123123, 
- 'test@test.nl', 
- '2018-03-01', 
- 3.00
-);
-ROLLBACK TRANSACTION;
+EXEC SP_PurchaseInsert
+     1,
+     'test@test.nl',
+     '2018-03-01',
+     3.2;
+EXEC SP_DeleteDemoData;
 
 -- [Scenario 05] : Film valt tussen abbonementsperiode's in.
 -- Result: Throw Error
-BEGIN TRANSACTION;
 EXEC SP_InsertDemoData;
-INSERT INTO Purchase
-VALUES
-(123123, 
- 'test@test.nl', 
- '2018-04-01', 
- 3.00
-);
-ROLLBACK TRANSACTION;
+EXEC SP_PurchaseInsert
+     1,
+     'test@test.nl',
+     '2018-04-01',
+     3.2;
+EXEC SP_DeleteDemoData;
 
 -- [Scenario 06] : Film valt in een periode waarin er geen einddag bekend is.
 -- Result: Success
-BEGIN TRANSACTION;
 EXEC SP_InsertDemoData;
-INSERT INTO Purchase
-VALUES
-(123123, 
- 'test@test.nl', 
- '2019-06-02', 
- 3.00
-);
-ROLLBACK TRANSACTION;
-
+EXEC SP_PurchaseInsert
+     1,
+     'test@test.nl',
+     '2019-06-02',
+     3.2;
+EXEC SP_DeleteDemoData;
 
 --  --------------------------------------------------------
 --  Cleanup
 --  --------------------------------------------------------
 DROP TRIGGER IF EXISTS TR_WatchMovieInPeriod;
 DROP PROCEDURE IF EXISTS SP_InsertDemoData;  
+DROP PROCEDURE IF EXISTS SP_DeleteDemoData;
 GO
